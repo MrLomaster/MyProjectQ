75ae8f3158e96aa906821641ae7bbc96485213ec
MrLomaster <timofeikalinin123@gmail.com>
2022-05-11 15:46:20 +0300

    MyProject v0.2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,8 +8,9 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->TLog->setText("1. Начать игру \n"
+    ui->TLog->setText("1. Создать персонажа \n"
                       "2. Продолжить");
+    ui->LogDB->hide();
     ui->BtnOk->hide();
 }
 
@@ -35,7 +36,20 @@ void MainWindow::on_BtnFarther_clicked()
         break;
 
     case 2:
-        NULL;
+        ui->Log->hide();
+        ui->LogDB->show();
+        ui->TLog->setText("Здраствуйте. Выберите персонажа");
+        db.setDatabaseName("C:/qt_creator/MyProjectQ/myproject.db");
+        bool ok=db.open();
+        if(!ok)
+        {
+            throw QObject::tr("Cannot open database");
+        }
+        QSqlQueryModel *modelTerminals = new QSqlQueryModel();
+            modelTerminals->setQuery("select * from Players",db);
+            modelTerminals->setHeaderData(1,Qt::Horizontal,"ФИО");
+            ui->LogDB->setModel(modelTerminals);
+            db.close();
         break;
     }
 }
@@ -46,15 +60,29 @@ void MainWindow::on_BtnOk_clicked()
     bool ok=db.open();
     if(!ok)
     {
-//        throw tr("Cannot open database");
+        throw tr("Cannot open database");
     }
 
     QSqlQuery query(db);
 
-    query.prepare("INSERT INTO Players (Name, LVL, ATK, DEF) VALUES (:Name, :LVL, :ATK, :DEF)");
-    query.bindValue(":Name", ui->LEdit->text());
-    query.bindValue(":LVL", 1);
-    query.bindValue(":ATK", 5);
-    query.bindValue(":DEF", 5);
+    query.prepare("INSERT INTO Players (Name, LVL, ATK, DEF) "
+                  "VALUES (:NameP, :LVLP, :ATKP, :DEFP)");
+    query.bindValue(":NameP", ui->LEdit->text());
+    query.bindValue(":LVLP", 1);
+    query.bindValue(":ATKP", 5);
+    query.bindValue(":DEFP", 5);
+
+//    query.prepare("INSERT INTO Info_Porsh (id_player, info_completeGuide) "
+//                  "VALUES (:id_playerP, :info_completeGuideP)");
+//    query.bindValue(":info_completeGuideP", 0);
+//    int id_buff = query.prepare("SELECT id FROM Players WHERE :NameP");
+//    query.bindValue(":id_playerP", id_buff);
+
+    query.exec();
+
     db.close();
+
+    ui->BtnOk->hide();
+    ui->TLog->setText("1. Создать персонажа \n"
+                      "2. Продолжить");
 }
